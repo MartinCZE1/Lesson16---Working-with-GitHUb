2f1fc28546e25d71bf28400a8cf6464edc0297b2
Martin Hercík <martinhercik16@gmail.com>
2023-12-20 11:17:33 +0100

    Task 2 - implemented second power of number.

--- src/main/java/cz/spsmb/hercik/gitTest/Main.java
+++ src/main/java/cz/spsmb/hercik/gitTest/Main.java
@@ -3,5 +3,8 @@ package cz.spsmb.hercik.gitTest;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int secondPowerNumber = MathUtils.secondPower(10);
+        System.out.println(secondPowerNumber);
     }
 }
---
+++ src/main/java/cz/spsmb/hercik/gitTest/MathUtils.java
@@ -0,0 +1,12 @@
+package cz.spsmb.hercik.gitTest;
+
+public class MathUtils {
+    /**
+     * Second power of number
+     * @param number
+     * @return
+     */
+     static int secondPower(int number) {
+        return number * number;
+    }
+}
